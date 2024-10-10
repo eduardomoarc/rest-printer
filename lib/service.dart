7b0561e3f8ca7a14f9cc7fcdcadbb3fd9d60ad4f
Eduardo Daniel Morón Arce <danix799@gmail.com>
2024-10-10 00:08:55 -0400

    integrated with printer

--- lib/service.dart
+++ lib/service.dart
@@ -1,3 +1,7 @@
+import 'dart:developer';
+
+import 'package:print_bluetooth_thermal/print_bluetooth_thermal.dart';
+import 'package:restprinter/printer.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 
@@ -7,7 +11,26 @@ class Service {
     router.get('/hello', (Request request) {
       return Response.ok('world!');
     });
+
+    router.post('/print', (Request request){
+      print();
+      return Response.ok("success printed");
+    });
     return router;
+  }
+
+  //https://pub.dev/packages/print_bluetooth_thermal
+  Future<void> print() async {
+    String mac = await ThermalPrinter().find();
+    log("printing on $mac");
+    await PrintBluetoothThermal.connect(macPrinterAddress: mac);
+    bool connectionStatus = await PrintBluetoothThermal.connectionStatus;
+    if (connectionStatus) {
+      String enter = '\n';
+      await PrintBluetoothThermal.writeBytes(enter.codeUnits);
+      await PrintBluetoothThermal.writeString(printText: PrintTextSize(size: 1, text: "test"));
+      final bool result = await PrintBluetoothThermal.disconnect;
+    }
   }
 
 }
